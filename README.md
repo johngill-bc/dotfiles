c7bb458fcffac61f7d4fb985093947e4d88baeb8
John Gill <john.gill@braincorp.com>
2020-05-15 13:15:02 -0700

    Add README and install script

---
+++ README.md
@@ -0,0 +1,12 @@
+# DOT Files
+This repository contains a set of environemnt setup scripts for:
+
+- vim
+- tmux
+- git
+- bash
+  - ssh key agent
+  - git aware prompt
+
+## Installation
+There is a `install.sh` script in the repository which will automatically setup all the files necessary. The script will prompt you for Git Name, Email and username to setup the global `.gitconfig` file.
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+GITDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+mkdir -p ~/.vim/colors/
+mkdir -p ~/.bash
+
+cp ${GITDIR}/gitconfig ~/.gitconfig
+cp ${GITDIR}/tmux.conf ~/tmux.conf
+cp ${GITDIR}/vimrc ~/.vimrc
+cp ${GITDIR}/solarized.vim ~/.vim/colors/solarized.vim
+cp ${GITDIR}/bash_profile ~/.my_bash_profile # avoid clobbering any other bash settings
+
+if ! grep -q "my_bash_profile" ~/.bashrc
+then
+    echo "source ~/.my_bash_profile" >> ~/.bashrc
+fi
+
+if [[ ! -d ~/.bash/git-aware-prompt ]]
+then
+    cd ~/.bash
+    git clone git://github.com/jimeh/git-aware-prompt.git
+fi
+
+read -p "GitHub Display Name: " gh_name
+read -p "GitHub Email: " gh_email
+read -p "Github username: " gh_user
+
+git config --global user.name "$gh_name"
+git config --global user.email "$gh_email"
+git config --global user.username "$gh_user"
